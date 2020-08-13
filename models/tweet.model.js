d29f17368232473ea4c1d6d23aa2d0ea1d3ea5bf
Cristian Toma <54420695+Cristian-Toma@users.noreply.github.com>
2020-08-13 17:52:53 -0600

    Update tweet.model.js

--- models/tweet.model.js
+++ models/tweet.model.js
@@ -5,7 +5,9 @@ const Schema = mongoose.Schema;
 
 const TweetSchema = Schema({
     date : Date,
+    likes: { type: Schema.Types.ObjectId, ref: "like" },
     creator : [{type:Schema.Types.ObjectId,ref:'user'}],
+    replies: [{ type: Schema.Types.ObjectId, ref: "reply" }],
     content: String
 });
 
