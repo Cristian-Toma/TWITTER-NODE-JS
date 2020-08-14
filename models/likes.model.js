8a22ca2d5e6be2bbe5557746c8783361755404c0
CrisT <andre2002madafaka@gmail.com>
2020-08-14 12:52:23 -0600

    Modeles updated

--- models/likes.model.js
+++ models/likes.model.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict'
 
 const Mongoose = require('mongoose');
@@ -10,16 +9,3 @@ const LikeSchema = Schema({
 });
 
 module.exports = Mongoose.model('like',LikeSchema);
-=======
-'use strict'
-
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const LikeSchema = Schema({
-  interactors: [{ type: Schema.Types.ObjectId, ref: "user" }],
-  likes: { type: Number, default: 0 },
-});
-
-module.exports = mongoose.model('like',LikeSchema);
->>>>>>> d29f17368232473ea4c1d6d23aa2d0ea1d3ea5bf
--- models/tweet.model.js
+++ models/tweet.model.js
@@ -5,20 +5,10 @@ const Schema = Mongoose.Schema;
 
 const TweetSchema = Schema({
     date : Date,
-<<<<<<< HEAD
-    likes: { type: Schema.Types.ObjectId, ref: 'like'},
-    creator : [{type:Schema.Types.ObjectId,ref: 'user'}],
-    replies: [{ type: Schema.Types.ObjectId, ref: 'reply' }],
-    content: String
-});
-
-module.exports = Mongoose.model('tweet',TweetSchema);
-=======
     likes: { type: Schema.Types.ObjectId, ref: "like" },
     creator : [{type:Schema.Types.ObjectId,ref:'user'}],
     replies: [{ type: Schema.Types.ObjectId, ref: "reply" }],
     content: String
 });
 
-module.exports = mongoose.model('tweet',TweetSchema);
->>>>>>> d29f17368232473ea4c1d6d23aa2d0ea1d3ea5bf
+module.exports = Mongoose.model('tweet',TweetSchema);
